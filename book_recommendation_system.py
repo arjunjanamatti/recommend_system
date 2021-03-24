38efef2157caa3d705f6834a31bd0d9b80750f6b
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-24 23:33:18 +0530

    book recommendation system

--- book_recommendation_system.py
+++ book_recommendation_system.py
@@ -24,3 +24,28 @@ print(f"There are {len(set(wikilinks))} unique wikilinks.")
 
 wikilinks_other_books = [link for link in wikilinks if link in book_index.keys()]
 print(f"There are {len(set(wikilinks_other_books))} unique wikilinks to other books.")
+
+from collections import Counter, OrderedDict
+
+
+def count_items(l):
+    """Return ordered dictionary of counts of objects in `l`"""
+
+    # Create a counter object
+    counts = Counter(l)
+
+    # Sort by highest count first and place in ordered dictionary
+    counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
+    counts = OrderedDict(counts)
+
+    return counts
+
+
+
+# Find set of wikilinks for each book and convert to a flattened list
+unique_wikilinks = list(chain(*[list(set(book[2])) for book in books]))
+
+wikilink_counts = count_items(unique_wikilinks)
+print(list(wikilink_counts.items())[:10])
+
+
