71988f3176e1401abc0dcfde85d1520225eb0328
arjunjanamatti <arjunjanamatti@gmail.com>
2021-04-06 22:21:58 +0530

    fill reviews and likes collections

--- fill_collections/fill_collection.py
+++ fill_collections/fill_collection.py
@@ -2,13 +2,41 @@ import pymongo
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 from collections import defaultdict
+import json
+
+def looping_json_files(files_list):
+    list_1 = []
+    for files in files_list:
+        with open(files) as file:
+            data = json.load(file)
+            list_1.append(data)
+    return list_1
+
 
 def main():
     'connect to the database'
     try:
-        c = MongoClient(host = 'localhost', port = 27017)
-        print('Connect to mongoDB')
-    except ConnectionFailure:
-        print('Cannot connect to mongoDB')
+        myclient = MongoClient()
+        mydb = myclient['real_reviews']
+        # collections is similar to tables in mongo dn
+        list_1 = looping_json_files(files_list)
+        for index, file in enumerate(files_list):
+            print(file)
+            print(file.split('.')[0])
+            my_collection = mydb[file.split('.')[0]]
+            my_collection.insert(doc_or_docs=list_1[index])
 
+
+        # my_collection = mydb['reviews']
+        #
+        # my_collection.insert(doc_or_docs = my_dictionary)
+        #
+        print('Data sent to the database')
+
+    except ConnectionFailure:
+        print('Failed to connect to mongoDB database')
+
+
+
+files_list = ['reviews.json', 'likes.json']
 main()
