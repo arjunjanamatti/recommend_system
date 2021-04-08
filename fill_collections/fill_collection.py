fb0462a0b624b1d082d9241f1335303f265a175f
arjunjanamatti <arjunjanamatti@gmail.com>
2021-04-08 21:25:48 +0530

    get the columns of merged dataframe

--- fill_collections/fill_collection.py
+++ fill_collections/fill_collection.py
@@ -16,7 +16,7 @@ def looping_json_files(files_list):
 
 
 def main():
-    'connect to the database'
+    'this function will send the json files to MongoDB'
     try:
         myclient = MongoClient()
         mydb = myclient['real_reviews']
@@ -78,54 +78,54 @@ def get_data():
         print('Failed to connect to mongo DB database')
 
 files_list = ['reviews_1.json','likes_1.json']
-main()
+# main()
 # get_data()
 #
-# myclient = MongoClient()
-# mydb = myclient['real_reviews']
-# list_1 = looping_json_files(files_list)
-# print(list_1)
-# tables_dictionary = {}
-# for index, file in enumerate(files_list):
-#     print(file)
-#     my_collection = mydb[file.split('.')[0]]
+myclient = MongoClient()
+mydb = myclient['real_reviews']
+list_1 = looping_json_files(files_list)
+print(list_1)
+tables_dictionary = {}
+for index, file in enumerate(files_list):
+    print(file)
+    my_collection = mydb[file.split('.')[0]]
+
+
+#     ### Find all the data
+#     print()
+#     print('Entire dataset: ')
+    list_data = my_collection.find()
+    df = pd.DataFrame(list(list_data))
+    tables_dictionary[file.split('.')[0]] = df
+print(tables_dictionary)
+    # print(list_data)
+#     for i in list_data:
+#         print(i)
 #
-#
-# #     ### Find all the data
-# #     print()
-# #     print('Entire dataset: ')
+#     try_list = defaultdict(list)
 #     list_data = my_collection.find()
-#     df = pd.DataFrame(list(list_data))
-#     tables_dictionary[file.split('.')[0]] = df
-# print(tables_dictionary)
-#     # print(list_data)
-# #     for i in list_data:
-# #         print(i)
-# #
-# #     try_list = defaultdict(list)
-# #     list_data = my_collection.find()
-# #
-# #     for j in list_data:
-# #         for k in j.keys():
-# #             try_list[k].append(j[k])
-# #         # try_list[j] .append(list_data[j])
-# #
-# #     # print(try_list)
-# #     col_list = list(try_list.keys())
-# #     print(len(col_list))
-# #     # df = pd.DataFrame([(k, v[0][0], v[0][1]) for k, v in try_list.items()],
-# #     #        columns=col_list)
-# #     # print(df.head())
-# #     df = pd.DataFrame(list(list_data))
-# #     print(df.head())
-# #
 #
-# df_1 = tables_dictionary['reviews_1']
-# df_1_approve = (df_1[df_1['isApprove']=='approved'])
-# df_2 = tables_dictionary['likes_1']
-# df_1_approve=df_1_approve.rename(columns={"_id": "resourceId"})
-# df_merge = df_1_approve.merge(df_2,how='left', on='resourceId')
-# print(df_merge.columns)
+#     for j in list_data:
+#         for k in j.keys():
+#             try_list[k].append(j[k])
+#         # try_list[j] .append(list_data[j])
+#
+#     # print(try_list)
+#     col_list = list(try_list.keys())
+#     print(len(col_list))
+#     # df = pd.DataFrame([(k, v[0][0], v[0][1]) for k, v in try_list.items()],
+#     #        columns=col_list)
+#     # print(df.head())
+#     df = pd.DataFrame(list(list_data))
+#     print(df.head())
+#
+
+df_1 = tables_dictionary['reviews_1']
+df_1_approve = (df_1[df_1['isApprove']=='approved'])
+df_2 = tables_dictionary['likes_1']
+df_1_approve=df_1_approve.rename(columns={"_id": "resourceId"})
+df_merge = df_1_approve.merge(df_2,how='left', on='resourceId')
+print(df_merge.columns)
 # df_merge_1 = df_merge[['resourceId', 'loc', 'createdAt_x', 'updatedAt_x','fromUserId_x']]
 # longitude = [_['coordinates'][0] for _ in df_merge_1['loc']]
 # latitude = [_['coordinates'][1] for _ in df_merge_1['loc']]
