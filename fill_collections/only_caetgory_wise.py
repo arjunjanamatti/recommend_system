283e5401d8a31389130ec1890c98d889c145cd5a
arjunjanamatti <arjunjanamatti@gmail.com>
2021-04-12 13:38:29 +0530

    created dummy likes

---
+++ fill_collections/only_caetgory_wise.py
@@ -0,0 +1,38 @@
+import pymongo
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+from collections import defaultdict
+import json
+import pandas as pd
+from datetime import timedelta
+from math import *
+import random
+
+def looping_json_files(files_list):
+    list_1 = []
+    for files in files_list:
+        with open(files) as file:
+            data = json.load(file)
+            list_1.append(data)
+    return list_1
+
+
+def SendJsonFilesToMongoDB(files_list):
+    'this function will send the json files to MongoDB'
+    try:
+        myclient = MongoClient()
+        mydb = myclient['real_reviews']
+        # collections is similar to tables in mongo dn
+        list_1 = looping_json_files(files_list)
+        for index, file in enumerate(files_list):
+            print(file)
+            print(file.split('.')[0])
+            my_collection = mydb[file.split('.')[0]]
+            my_collection.insert(doc_or_docs=list_1[index])
+
+        print('Data sent to the database')
+
+    except ConnectionFailure:
+        print('Failed to connect to mongoDB database')
+
+files_list = ['reviews_1.json','likes_1.json']
