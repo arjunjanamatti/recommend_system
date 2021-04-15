9a4f31acc95acb5ddb1c445506802589fbb3101f
arjunjanamatti <arjunjanamatti@gmail.com>
2021-04-15 23:39:01 +0530

    added review_id_tags to the products each item

--- fill_collections/split_review_id_list.py
+++ fill_collections/split_review_id_list.py
@@ -1,6 +1,6 @@
-
+import json
 ran_list = list(range(1,100))
-
+import pickle
 from itertools import islice
 from random import randint
 
@@ -13,7 +13,17 @@ def random_chunk(li, min_chunk=10, max_chunk=11):
         else:
             break
 
-a = (list(random_chunk(ran_list)))
+with open('resourceId.pickle', 'rb') as f:
+    resourceId = pickle.load(f)
 
-print(a)
-print(len(a))
+review_id_tags = list(random_chunk(resourceId))
+print(review_id_tags)
+print(len(review_id_tags))
+
+with open('products.json') as file:
+    products = json.load(file)
+products_list = []
+for index, pro in enumerate(products):
+    pro['review_id_tags'] = review_id_tags[index]
+    products_list.append(pro)
+print(products_list)
