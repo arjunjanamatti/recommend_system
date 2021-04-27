ccad05c66d370b49e7fb441de4c68e0b972254d9
arjunjanamatti <arjunjanamatti@gmail.com>
2021-04-27 21:51:45 +0530

    append to already existing id

--- fill_collections/create_block_users.py
+++ fill_collections/create_block_users.py
@@ -34,12 +34,33 @@ new_dict = [{
         "createdAt": "2021-04-26T17:57:58.192Z",
         "updatedAt": "2021-04-26T17:57:58.192Z",
         "__v": 0
+    },
+    {
+        "_id": "6086ff261f6ee05e62e109c10",
+        "blockUserId": "5fe0d004fe08e8c0191a7816",
+        "fromUserId": "5fdf6bbcfe08e8c0191a7817",
+        "createdAt": "2021-04-26T17:57:58.192Z",
+        "updatedAt": "2021-04-26T17:57:58.192Z",
+        "__v": 0
     }
 ]
 try_dict = {}
 for new in new_dict:
-    try_dict[new['_id']] = []
-    try_dict[new['_id']].append(new['blockUserId'])
-    try_dict[new['_id']].append(new['fromUserId'])
+    if new['_id'] not in try_dict.keys():
+        try_dict[new['_id']] = []
+        try_dict[new['_id']].append(new['blockUserId'])
+        try_dict[new['_id']].append(new['fromUserId'])
+    else:
+        try_dict[new['_id']].append(new['blockUserId'])
+        try_dict[new['_id']].append(new['fromUserId'])
 
 print(try_dict)
+
+blocked_id_list = []
+for val in try_dict.values():
+    if "5fdf6bbcfe08e8c0191a7805" in val:
+        print(val)
+
+    print([blocked_id_list.append(x) for x in val if x == '5fdf6bbcfe08e8c0191a7805'])
+
+print(blocked_id_list)
