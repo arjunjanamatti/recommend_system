be2ab275f9e7b8fe040b207d257f6a643bc5761f
arjunjanamatti <arjunjanamatti@gmail.com>
2021-04-27 22:46:08 +0530

    run the script for single block

--- fill_collections/create_block_users.py
+++ fill_collections/create_block_users.py
@@ -10,42 +10,17 @@ with open('resourceId.pickle', 'rb') as f:
     resourceId = pickle.load(f)
 
 nested_dict = []
-sample_dict =   {
-    "_id": "6086ff261f6ee05e62e109c9",
+sample_dict =   [
+    {"_id": "6086ff261f6ee05e62e109c9",
     "blockUserId": "5fe0d004fe08e8c0191a7806",
     "fromUserId": "5fdf6bbcfe08e8c0191a7805",
     "createdAt": "2021-04-26T17:57:58.192Z",
     "updatedAt": "2021-04-26T17:57:58.192Z",
     "__v": 0
-}
+}]
 
-new_dict = [{
-    "_id": "6086ff261f6ee05e62e109c9",
-    "blockUserId": "5fe0d004fe08e8c0191a7806",
-    "fromUserId": "5fdf6bbcfe08e8c0191a7805",
-    "createdAt": "2021-04-26T17:57:58.192Z",
-    "updatedAt": "2021-04-26T17:57:58.192Z",
-    "__v": 0
-},
-    {
-        "_id": "6086ff261f6ee05e62e109c10",
-        "blockUserId": "5fe0d004fe08e8c0191a7816",
-        "fromUserId": "5fdf6bbcfe08e8c0191a7815",
-        "createdAt": "2021-04-26T17:57:58.192Z",
-        "updatedAt": "2021-04-26T17:57:58.192Z",
-        "__v": 0
-    },
-    {
-        "_id": "6086ff261f6ee05e62e109c10",
-        "blockUserId": "5fe0d004fe08e8c0191a7816",
-        "fromUserId": "5fdf6bbcfe08e8c0191a7817",
-        "createdAt": "2021-04-26T17:57:58.192Z",
-        "updatedAt": "2021-04-26T17:57:58.192Z",
-        "__v": 0
-    }
-]
 try_dict = {}
-for new in new_dict:
+for new in sample_dict:
     if new['_id'] not in try_dict.keys():
         try_dict[new['_id']] = []
         try_dict[new['_id']].append(new['blockUserId'])
@@ -59,8 +34,8 @@ print(try_dict)
 blocked_id_list = []
 for val in try_dict.values():
     if "5fdf6bbcfe08e8c0191a7805" in val:
-        print(val)
-
-    print([blocked_id_list.append(x) for x in val if x == '5fdf6bbcfe08e8c0191a7805'])
+        a = [blocked_id_list.append(x) for x in val]
 
 print(blocked_id_list)
+
+
