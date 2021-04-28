9a95a33fd9679f9dda8f93cd9bf6af37398e2711
arjunjanamatti <arjunjanamatti@gmail.com>
2021-04-28 23:36:20 +0530

    single user blocks multiple users data sent to MongoDB

--- fill_collections/create_block_users.py
+++ fill_collections/create_block_users.py
@@ -7,63 +7,78 @@ from pymongo import MongoClient
 import pandas as pd
 
 ### SEND A SAMPLE DICTIONARY TO MONGODB
-# with open('resourceId.pickle', 'rb') as f:
-#     resourceId = pickle.load(f)
-#
-# nested_dict = []
-# sample_dict =   [
-#     {"_id": "6086ff261f6ee05e62e109c9",
-#     "blockUserId": "5fdf6bbcfe08e8c0191a7814",
-#     "fromUserId": "5fdf6bbcfe08e8c0191a7805",
-#     "createdAt": "2021-04-26T17:57:58.192Z",
-#     "updatedAt": "2021-04-26T17:57:58.192Z",
-#     "__v": 0
-# }]
-#
-# try_dict = {}
-# for new in sample_dict:
-#     if new['_id'] not in try_dict.keys():
-#         try_dict[new['_id']] = []
-#         try_dict[new['_id']].append(new['blockUserId'])
-#         try_dict[new['_id']].append(new['fromUserId'])
-#     else:
-#         try_dict[new['_id']].append(new['blockUserId'])
-#         try_dict[new['_id']].append(new['fromUserId'])
-#
-# print(try_dict)
-#
-# blocked_id_list = []
-# for val in try_dict.values():
-#     if "5fdf6bbcfe08e8c0191a7805" in val:
-#         a = [blocked_id_list.append(x) for x in val]
-#
-# print(blocked_id_list)
-# with open("blockusers.json", "w") as fp:
-#     json.dump(sample_dict , fp, indent=4)
-#
-# from fill_collection import *
-#
-# files_list = ['blockusers.json']
-# SendJsonFilesToMongoDB(files_list)
+with open('resourceId.pickle', 'rb') as f:
+    resourceId = pickle.load(f)
 
-### GET THE DATA FROM MONGODB TO DICTIONARY
-myclient = MongoClient(host=None, port=None)
-mydb = myclient['real_reviews']
-coll = mydb['blockusers']
-cur = coll.find()
-block_users_dict_list = [doc for doc in cur]
-print(block_users_dict_list)
-blocked_id_list = []
+nested_dict = []
+sample_dict =   [
+    {"_id": "6086ff261f6ee05e62e109c9",
+    "blockUserId": "5fdf6bbcfe08e8c0191a7814",
+    "fromUserId": "5fdf6bbcfe08e8c0191a7805",
+    "createdAt": "2021-04-26T17:57:58.192Z",
+    "updatedAt": "2021-04-26T17:57:58.192Z",
+    "__v": 0
+},
+    {"_id": "6086ff261f6ee05e62e109c10",
+     "blockUserId": "5fdf6bbcfe08e8c0191a7820",
+     "fromUserId": "5fdf6bbcfe08e8c0191a7805",
+     "createdAt": "2021-04-26T17:57:58.192Z",
+     "updatedAt": "2021-04-26T17:57:58.192Z",
+     "__v": 0
+     },
+    {"_id": "6086ff261f6ee05e62e109c11",
+    "blockUserId": "5fdf6bbcfe08e8c0191a7826",
+    "fromUserId": "5fdf6bbcfe08e8c0191a7805",
+    "createdAt": "2021-04-26T17:57:58.192Z",
+    "updatedAt": "2021-04-26T17:57:58.192Z",
+    "__v": 0
+}
+]
 
 try_dict = {}
-for new in block_users_dict_list:
-    try_dict[new['_id']] = []
-    try_dict[new['_id']].append(new['blockUserId'])
-    try_dict[new['_id']].append(new['fromUserId'])
+for new in sample_dict:
+    if new['_id'] not in try_dict.keys():
+        try_dict[new['_id']] = []
+        try_dict[new['_id']].append(new['blockUserId'])
+        try_dict[new['_id']].append(new['fromUserId'])
+    else:
+        try_dict[new['_id']].append(new['blockUserId'])
+        try_dict[new['_id']].append(new['fromUserId'])
 
 print(try_dict)
 
+blocked_id_list = []
 for val in try_dict.values():
-    if '5fdf6bbcfe08e8c0191a7814' in val:
-        print([v for v in val if v != '5fdf6bbcfe08e8c0191a7814'][-1])
-    print(val)
+    if "5fdf6bbcfe08e8c0191a7805" in val:
+        a = [blocked_id_list.append(x) for x in val]
+
+print(blocked_id_list)
+with open("blockusers.json", "w") as fp:
+    json.dump(sample_dict , fp, indent=4)
+
+from fill_collection import *
+
+files_list = ['blockusers.json']
+SendJsonFilesToMongoDB(files_list)
+
+### GET THE DATA FROM MONGODB TO DICTIONARY
+# myclient = MongoClient(host=None, port=None)
+# mydb = myclient['real_reviews']
+# coll = mydb['blockusers']
+# cur = coll.find()
+# block_users_dict_list = [doc for doc in cur]
+# print(block_users_dict_list)
+# blocked_id_list = []
+#
+# try_dict = {}
+# for new in block_users_dict_list:
+#     try_dict[new['_id']] = []
+#     try_dict[new['_id']].append(new['blockUserId'])
+#     try_dict[new['_id']].append(new['fromUserId'])
+#
+# print(try_dict)
+#
+# for val in try_dict.values():
+#     if '5fdf6bbcfe08e8c0191a7814' in val:
+#         print([v for v in val if v != '5fdf6bbcfe08e8c0191a7814'][-1])
+#     print(val)
