654022646146ee0d2e89132aa83aa2e7bbd34ef3
arjunjanamatti <arjunjanamatti@gmail.com>
2021-04-30 22:32:05 +0530

    added the libraries

---
+++ fill_collections/main_recommend.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+import json
+import pandas as pd
+from datetime import timedelta
+from math import *
+from flask import Flask, request
+import numpy as np
+import time
+
+
