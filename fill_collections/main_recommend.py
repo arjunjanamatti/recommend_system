072b14b4cb3b874f85d3914a3282a2b6a4063bfa
arjunjanamatti <arjunjanamatti@gmail.com>
2021-04-30 22:34:58 +0530

    added the tabledictionary

--- fill_collections/main_recommend.py
+++ fill_collections/main_recommend.py
@@ -22,6 +22,20 @@ class recommend_results:
             self.try_dict[new['_id']].append(new['blockUserId'])
             self.try_dict[new['_id']].append(new['fromUserId'])
 
+    def GetTableDictionary(self):
+        myclient = MongoClient(host='localhost', port=27017)
+        mydb = myclient['real_reviews']
+        # list_1 = self.looping_json_files()
+        self.files_list = ['reviews.json', 'likes.json']
+        # self.files_list = ['reviews_1.json', 'likes_1.json']
+        self.tables_dictionary = {}
+        for index, file in enumerate(self.files_list):
+            my_collection = mydb[file.split('.')[0]]
+            list_data = my_collection.find()
+            df = pd.DataFrame(list(list_data))
+            self.tables_dictionary[file.split('.')[0]] = df
+        return self.tables_dictionary
+
     
     pass
 
