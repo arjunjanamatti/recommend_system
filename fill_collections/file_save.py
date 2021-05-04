2f4b25e6bd4a3eb7a19d170154cf2e2729bc3eff
arjunjanamatti <arjunjanamatti@gmail.com>
2021-05-04 20:05:55 +0530

    added makedir method

--- fill_collections/file_save.py
+++ fill_collections/file_save.py
@@ -2,15 +2,28 @@ import os
 from flask import Flask, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/'
+# UPLOAD_FOLDER = 'saved_videos'
 app = Flask(__name__)
 
-@app.route('/video/upload', methods=['GET', 'POST'])
+def makedir(file):
+    try:
+        os.mkdir(file)
+
+    except Exception as e:
+        print(f'Execption in making directory: {e}')
+
+    pass
+
+@app.route('/savefile', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
+        print(file.filename)
+        foldername = file.filename
+        makedir(foldername.split('.')[0])
         filename = secure_filename(file.filename)
-        file.save(UPLOAD_FOLDER, filename)
+        file.save(foldername, filename)
+        return {'Result': "Uploaded file"}
     pass
 
 if __name__ == '__main__':
