28ae4008b591eef311f9bd1bd475151344aa828a
arjunjanamatti <arjunjanamatti@gmail.com>
2021-05-13 20:25:57 +0530

    sample businesses

--- fill_collections/create_dummy_business.py
+++ fill_collections/create_dummy_business.py
@@ -1,2 +1,27 @@
 
 business_name_list = ['samsung', 'apple', 'oppo', 'toshiba', 'lenovo', 'acer', 'nokia', 'blackberry']
+
+sample_business = {
+    "_id": "609681f4fdbd561c6cea96c4",
+    "loc": {
+        "coordinates": [
+            66.3,
+            33.63
+        ],
+        "type": "Point"
+    },
+    "image": [
+        "file-1620476390867.jpg"
+    ],
+    "isDeleted": True,
+    "isActive": True,
+    "name": "Tesla",
+    "slug": "tesla",
+    "description": "Car Manufacturer",
+    "type": "Car",
+    "coverImage": "file-1620476394002.jpg",
+    "addedByUserId": "5fc7c1e7a3fb189c35bb8945",
+    "createdAt": "2021-05-08T12:20:04.502Z",
+    "updatedAt": "2021-05-08T12:20:56.895Z",
+    "__v": 1
+}
