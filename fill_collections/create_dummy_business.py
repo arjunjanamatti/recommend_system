81218edaff646f1bf2143b09e963bb2d45de7236
arjunjanamatti <arjunjanamatti@gmail.com>
2021-05-13 22:27:30 +0530

    random date function to create dates

--- fill_collections/create_dummy_business.py
+++ fill_collections/create_dummy_business.py
@@ -1,5 +1,7 @@
 
 import random
+import time
+from datetime import datetime
 
 business_name_list = ['samsung', 'apple', 'oppo', 'toshiba', 'lenovo', 'acer', 'nokia', 'blackberry']
 
@@ -28,15 +30,24 @@ sample_business = {
     "__v": 1
 }
 
+nested_dict = []
+
+def randomDate(start, end):
+    frmt = '%d-%m-%Y %H:%M:%S'
+
+    stime = time.mktime(time.strptime(start, frmt))
+    etime = time.mktime(time.strptime(end, frmt))
+
+    ptime = stime + random.random() * (etime - stime)
+    dt = datetime.fromtimestamp(time.mktime(time.localtime(ptime)))
+    return dt
+
+
 for i in range(900):
     new_dict = sample_business.copy()
     # 'fromUserId': '5fdf6bbcfe08e8c0191a7805'
     new_dict['_id'] = new_dict['_id'][:19]+str(799386+i)
-    new_dict['title'] = random.choice(title_list)
-    new_dict['loc'] = {'coordinates':cordinates_nested[i], 'type': 'Point'}
-    new_dict['rating'] = random.uniform(1,10)
-    new_dict['categoryId'] =  random.choice(categores_id_list)
-    new_dict['fromUserId'] = random.choice(user_id_list)
+    new_dict['name'] = random.choice(business_name_list)
     new_dict['createdAt'] = (randomDate("20-01-2021 13:30:00", "23-04-2021 04:50:34")).strftime('%d-%m-%YT%H:%M:%S')
     new_dict['updatedAt'] = (randomDate("20-01-2021 13:30:00", "23-04-2021 04:50:34")).strftime('%d-%m-%YT%H:%M:%S')
     # new_dict = sample_dict.copy()
