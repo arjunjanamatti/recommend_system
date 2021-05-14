d66e6a087699307dbbba69120e8248a390f23b29
arjunjanamatti <arjunjanamatti@gmail.com>
2021-05-14 15:48:22 +0530

    added products.json

--- fill_collections/create_dummy_products.py
+++ fill_collections/create_dummy_products.py
@@ -6,6 +6,8 @@ import pickle
 from pymongo import MongoClient
 import pandas as pd
 from pymongo.errors import ConnectionFailure
+from itertools import islice
+from random import randint
 
 def random_chunk(li, min_chunk=10, max_chunk=11):
     it = iter(li)
@@ -31,39 +33,41 @@ for index, pro in enumerate(products):
     products_list.append(pro)
 print(products_list)
 
-with open("products_1.json", "w") as fp:
-    json.dump(products_list , fp, indent=4)
+print(len(products_list))
 
-def looping_json_files(files_list):
-    list_1 = []
-    for files in files_list:
-        with open(files) as file:
-            data = json.load(file)
-            list_1.append(data)
-    return list_1
-
-def SendJsonFilesToMongoDB(files_list):
-    'this function will send the json files to MongoDB'
-    try:
-        myclient = MongoClient()
-        mydb = myclient['real_reviews']
-        # collections is similar to tables in mongo dn
-        list_1 = looping_json_files(files_list)
-        for index, file in enumerate(files_list):
-            print(file)
-            print(file.split('.')[0])
-            my_collection = mydb[file.split('.')[0]]
-            my_collection.insert(doc_or_docs=list_1[index])
-
-        print('Data sent to the database')
-
-    except ConnectionFailure:
-        print('Failed to connect to mongoDB database')
-
-files_list = ['products_1.json']
-
-# only first time use the below function
-try:
-    SendJsonFilesToMongoDB(files_list)
-except Exception as e:
-    print(e)
+# with open("products_1.json", "w") as fp:
+#     json.dump(products_list , fp, indent=4)
+#
+# def looping_json_files(files_list):
+#     list_1 = []
+#     for files in files_list:
+#         with open(files) as file:
+#             data = json.load(file)
+#             list_1.append(data)
+#     return list_1
+#
+# def SendJsonFilesToMongoDB(files_list):
+#     'this function will send the json files to MongoDB'
+#     try:
+#         myclient = MongoClient()
+#         mydb = myclient['real_reviews']
+#         # collections is similar to tables in mongo dn
+#         list_1 = looping_json_files(files_list)
+#         for index, file in enumerate(files_list):
+#             print(file)
+#             print(file.split('.')[0])
+#             my_collection = mydb[file.split('.')[0]]
+#             my_collection.insert(doc_or_docs=list_1[index])
+#
+#         print('Data sent to the database')
+#
+#     except ConnectionFailure:
+#         print('Failed to connect to mongoDB database')
+#
+# files_list = ['products_1.json']
+#
+# # only first time use the below function
+# try:
+#     SendJsonFilesToMongoDB(files_list)
+# except Exception as e:
+#     print(e)
