045da06c336c719050e0c9ed02074634f5dc1ed4
arjunjanamatti <arjunjanamatti@gmail.com>
2021-05-14 14:36:12 +0530

    updated dummy likes script with function

--- fill_collections/create_dummy_likes.py
+++ fill_collections/create_dummy_likes.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import pickle
 from pymongo import MongoClient
 import pandas as pd
+from pymongo.errors import ConnectionFailure
 
 with open('resourceId.pickle', 'rb') as f:
     resourceId = pickle.load(f)
@@ -50,7 +51,32 @@ with open("likes_2.json", "w") as fp:
 df = pd.read_json("likes_2.json")
 print(df['resourceId'].value_counts())
 
-from fill_collection import *
+def looping_json_files(files_list):
+    list_1 = []
+    for files in files_list:
+        with open(files) as file:
+            data = json.load(file)
+            list_1.append(data)
+    return list_1
+
+
+def SendJsonFilesToMongoDB(files_list):
+    'this function will send the json files to MongoDB'
+    try:
+        myclient = MongoClient()
+        mydb = myclient['real_reviews']
+        # collections is similar to tables in mongo dn
+        list_1 = looping_json_files(files_list)
+        for index, file in enumerate(files_list):
+            print(file)
+            print(file.split('.')[0])
+            my_collection = mydb[file.split('.')[0]]
+            my_collection.insert(doc_or_docs=list_1[index])
+
+        print('Data sent to the database')
+
+    except ConnectionFailure:
+        print('Failed to connect to mongoDB database')
 
 files_list = ['likes_2.json']
 SendJsonFilesToMongoDB(files_list)
