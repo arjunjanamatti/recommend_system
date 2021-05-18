41ce5a12f97b802539a1b642817de5c7c2435f70
arjunjanamatti <arjunjanamatti@gmail.com>
2021-05-18 21:29:01 +0530

    checked the script and working

--- fill_collections/send_new_to_mongodb_table.py
+++ fill_collections/send_new_to_mongodb_table.py
@@ -4,7 +4,7 @@ def TrialFunctions():
         return 10 + 20
 
 def UpdateKeys(id_list):
-        for id in id_list:
+        for id in id_list[:int(0.5*len(id_list))]:
                 coll.update({'_id': f'{id}'}, {'$set': {'testing_new_key': f'{str(TrialFunctions())}'}})
         pass
 
@@ -14,10 +14,14 @@ coll = mydb['reviews']
 # mongo_db = myclient.real_reviews
 # allkeyvalues = mongo_db.reviews.find({},{'testing_new_key': 1})
 allkeyvalues = coll.find({},{'testing_new_key': 1})
+non_available_id = []
 for result in allkeyvalues:
         if 'testing_new_key' not in result.keys():
                 print(result)
-                print(result['_id'])
+                non_available_id.append(result['_id'])
+
+UpdateKeys(non_available_id)
+
 
 # # working to find all _id in the table
 # list_id = coll.find().distinct('_id')
