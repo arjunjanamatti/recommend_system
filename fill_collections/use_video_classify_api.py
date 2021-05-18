f448e26de9597ee725f67518db33f3d4fe78c380
arjunjanamatti <arjunjanamatti@gmail.com>
2021-05-18 23:31:26 +0530

    testing with use_api script

--- fill_collections/use_video_classify_api.py
+++ fill_collections/use_video_classify_api.py
@@ -3,34 +3,17 @@ import os
 import json
 from glob import glob
 
-# # curl -i -X POST -H "Content-Type: multipart/form-data"
-# # -F "data=@test.mp3" http://mysuperserver/media/1234/upload/
-# command = ['curl', "-i", '-X', 'POST', '-H', 'Content-Type: multipart/form-data', '-F',
-#            'file=@C:/Users/Arjun Janamatti/PycharmProjects/try_recommend/fill_collections/arnold.mp4',
-#            'http://127.0.0.1:5050/video/upload']
-# video_to_audio = subprocess.Popen(command, shell=True)
-# print(f'video_to_audio: {video_to_audio}')
-# print(f'Result: {video_to_audio.communicate()[0]}')
-# print('Upload done')
-
-# url = 'http://127.0.0.1:5050/video/upload'
-# myobj = {'file': '@C:/Users/Arjun Janamatti/PycharmProjects/try_recommend/fill_collections/arnold.mp4'}
-#
-# x = requests.post(url, data = myobj)
-#
-# print(x.status_code, x.reason)
-
-# with open('C:/Users/Arjun Janamatti/PycharmProjects/try_recommend/fill_collections/arnold.mp4', 'r') as f:
-#     r = requests.post('http://127.0.0.1:5050/video/upload', files={'C:/Users/Arjun Janamatti/PycharmProjects/try_recommend/fill_collections/arnold.mp4': f})
-#
-# print(r.status_code, r.reason)
-import time
 from pymongo import MongoClient
 def GetSpeech(file_location, host):
     my_file = {'file': open(os.path.realpath(file_location), 'rb')}
     vids = requests.post(host, files=my_file)
     result_dict = json.loads(vids.text)
     return result_dict['Video speech'], result_dict['Profane words']
+
+def UpdateKeys(id_list, file_location, host):
+    for id in id_list[:int(0.5 * len(id_list))]:
+        coll.update({'_id': f'{id}'}, {'$set': {'testing_new_key': f'{str(GetSpeech(file_location, host))}'}})
+    pass
 
 host = 'http://127.0.0.1:5050/text-speech'
 myclient = MongoClient(host=None, port=None)
@@ -47,10 +30,12 @@ reviews_location = 'C:/Users/Arjun Janamatti/Downloads/reviews'
 reviews_list = glob('{}/*'.format(reviews_location))
 print(reviews_list)
 
-for review in reviews_list[:1]:
+for review in reviews_list[:2]:
     first_video_list = glob('{}/*'.format(f'{review}/video'))
     print(first_video_list[-1])
     file_location = first_video_list
+    UpdateKeys(review_id_no_speech, file_location, host)
+
 
 # file_location = 'C:/Users/Arjun Janamatti/PycharmProjects/try_recommend/fill_collections/arnold.mp4'
 # host = 'http://127.0.0.1:5050/text-speech'
