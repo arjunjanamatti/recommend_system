b5a51b26769ac66c9d201a70e858037ba6336539
arjunjanamatti <arjunjanamatti@gmail.com>
2021-05-19 15:20:33 +0530

    working script using only functions

--- fill_collections/use_video_classify_api.py
+++ fill_collections/use_video_classify_api.py
@@ -2,8 +2,8 @@ import requests
 import os
 import json
 from glob import glob
-
 from pymongo import MongoClient
+
 def GetSpeech(file_location, host):
     my_file = {'file': open(os.path.realpath(file_location), 'rb')}
     vids = requests.post(host, files=my_file)
@@ -25,7 +25,7 @@ def GetReviewsWithNoSpeech():
      if 'full_speech' not in result]
     return review_id_no_speech
 
-def GetUpdatedReviewId(reviews_location):
+def GetUpdatedReviewId(reviews_location, review_id_no_speech):
     reviews_list = glob('{}/*'.format(reviews_location))
     update_review_list = []
 
@@ -41,40 +41,47 @@ host = 'http://127.0.0.1:5050/text-speech'
 myclient = MongoClient(host=None, port=None)
 mydb = myclient['real_reviews']
 coll = mydb['reviews']
-review_id_list = coll.find({},{'full_speech': 1})
-review_id_no_speech = []
-for result in review_id_list:
-    if 'full_speech' not in result:
-        review_id_no_speech.append(f'{result["_id"]}')
-
+# review_id_list = coll.find({},{'full_speech': 1})
+# review_id_no_speech = []
+# for result in review_id_list:
+#     if 'full_speech' not in result:
+#         review_id_no_speech.append(f'{result["_id"]}')
+#
+#
+# reviews_location = 'C:/Users/Arjun Janamatti/Downloads/reviews'
+# reviews_list = glob('{}/*'.format(reviews_location))
+# # print(reviews_list)
+# # print(review_id_no_speech)
+#
+# update_review_list = []
+#
+# for review in reviews_list:
+#     review_id = review.split('\\')[-1]
+#     # print(review_id)
+#     if review_id in review_id_no_speech:
+#         update_review_list.append(review)
+#
+# # print(update_review_list)
 
 reviews_location = 'C:/Users/Arjun Janamatti/Downloads/reviews'
-reviews_list = glob('{}/*'.format(reviews_location))
-# print(reviews_list)
-# print(review_id_no_speech)
+review_id_no_speech = GetReviewsWithNoSpeech()
+update_review_list = GetUpdatedReviewId(reviews_location, review_id_no_speech)
 
-update_review_list = []
-
-for review in reviews_list:
-    review_id = review.split('\\')[-1]
-    # print(review_id)
-    if review_id in review_id_no_speech:
-        update_review_list.append(review)
-
-# print(update_review_list)
-
-for review in update_review_list:
-    first_video_list = glob('{}/*'.format(f'{review}/video'))
-    print(first_video_list[-1])
-    file_location = first_video_list[-1]
-    speech_text, profane_text = GetSpeech(file_location, host)
-    print(speech_text)
-    print(type(speech_text))
-    print(profane_text)
-    print(type(profane_text))
-    review_id = review.split('\\')[-1]
-    UpdateKeys(review_id, file_location, host, speech_text, profane_text)
-    print(f'update on Review_id: {review_id} completed !!!')
+if len(update_review_list) > 0:
+    for review in update_review_list:
+        first_video_list = glob('{}/*'.format(f'{review}/video'))
+        print(first_video_list[-1])
+        file_location = first_video_list[-1]
+        speech_text, profane_text = GetSpeech(file_location, host)
+        print(speech_text)
+        print(type(speech_text))
+        print(profane_text)
+        print(type(profane_text))
+        review_id = review.split('\\')[-1]
+        UpdateKeys(review_id, file_location, host, speech_text, profane_text)
+        print(f'update on Review_id: {review_id} completed !!!')
+else:
+    pass
 
 
 # file_location = 'C:/Users/Arjun Janamatti/PycharmProjects/try_recommend/fill_collections/arnold.mp4'
