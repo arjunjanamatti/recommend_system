a8234d39cf73acf0344018bc3d1b9d16c9901ea3
arjunjanamatti <arjunjanamatti@gmail.com>
2021-05-21 14:47:18 +0530

    added class with blockusers and tabledictionary methods

--- fill_collections/try_similar_trending_topics.py
+++ fill_collections/try_similar_trending_topics.py
@@ -11,6 +11,39 @@ from flask import Flask, request
 import numpy as np
 import time
 
-files_list = ['reviews_1.json','likes_1.json']
-
 app = Flask(__name__)
+
+class trend_results:
+    def __init__(self):
+        self.top_review_last_week = {}
+        self.top_user_last_week = {}
+        self.popular_review_last_month = {}
+        self.popular_user_last_month = {}
+        pass
+
+    def GetBlockUsersData(self):
+        myclient = MongoClient(host=None, port=None)
+        mydb = myclient['real_reviews']
+        coll = mydb['blockusers']
+        cur = coll.find()
+        block_users_dict_list = [doc for doc in cur]
+
+        self.try_dict = {}
+        for new in block_users_dict_list:
+            self.try_dict[new['_id']] = []
+            self.try_dict[new['_id']].append(new['blockUserId'])
+            self.try_dict[new['_id']].append(new['fromUserId'])
+
+    def GetTableDictionary(self):
+        myclient = MongoClient(host='localhost', port=27017)
+        mydb = myclient['real_reviews']
+        # list_1 = self.looping_json_files()
+        # self.files_list = ['reviews.json', 'likes.json']
+        self.files_list = ['reviews_2.json', 'likes_2.json']
+        self.tables_dictionary = {}
+        for index, file in enumerate(self.files_list):
+            my_collection = mydb[file.split('.')[0]]
+            list_data = my_collection.find()
+            df = pd.DataFrame(list(list_data))
+            self.tables_dictionary[file.split('.')[0]] = df
+        return self.tables_dictionary
