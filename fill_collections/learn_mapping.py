b4208f35d96d6a8dbb504e2101335056823da103
arjunjanamatti <arjunjanamatti@gmail.com>
2021-05-24 23:24:22 +0530

    comparing map and for loop

--- fill_collections/learn_mapping.py
+++ fill_collections/learn_mapping.py
@@ -16,7 +16,18 @@ cur = coll.find()
 block_users_dict_list = [doc for doc in cur]
 
 try_dict = {}
-for new in block_users_dict_list:
+def get_data_block_users(new):
     try_dict[new['_id']] = []
     try_dict[new['_id']].append(new['blockUserId'])
     try_dict[new['_id']].append(new['fromUserId'])
+    return try_dict
+
+print(dict(map(get_data_block_users, block_users_dict_list)))
+
+try_dict_1 = {}
+for new in block_users_dict_list:
+    try_dict_1[new['_id']] = []
+    try_dict_1[new['_id']].append(new['blockUserId'])
+    try_dict_1[new['_id']].append(new['fromUserId'])
+
+print(try_dict_1)
