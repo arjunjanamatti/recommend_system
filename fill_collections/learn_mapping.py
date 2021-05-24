d5a7722066efd0743a74dfce518b6ffdb199b162
arjunjanamatti <arjunjanamatti@gmail.com>
2021-05-24 23:19:21 +0530

    trying for blockusers

--- fill_collections/learn_mapping.py
+++ fill_collections/learn_mapping.py
@@ -1,8 +1,22 @@
-num_list = list(range(1,101))
+# num_list = list(range(11,20))
+#
+# def square(num):
+#     return num**2
+#
+# square_num = map(square, num_list)
+#
+# print(list(square_num))
 
-def square(num):
-    return num**2
+from pymongo import MongoClient
 
-square_num = map(square, num_list)
+myclient = MongoClient(host=None, port=None)
+mydb = myclient['real_reviews']
+coll = mydb['blockusers']
+cur = coll.find()
+block_users_dict_list = [doc for doc in cur]
 
-print(square_num)
+try_dict = {}
+for new in block_users_dict_list:
+    try_dict[new['_id']] = []
+    try_dict[new['_id']].append(new['blockUserId'])
+    try_dict[new['_id']].append(new['fromUserId'])
