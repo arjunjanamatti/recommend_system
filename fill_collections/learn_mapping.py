e2456eee5e6c8b8d2639ba73ecbe069fb7b5de7e
arjunjanamatti <arjunjanamatti@gmail.com>
2021-05-24 23:36:16 +0530

    comparing map and for loop

--- fill_collections/learn_mapping.py
+++ fill_collections/learn_mapping.py
@@ -24,8 +24,13 @@ def get_data_block_users(new):
 
 print(block_users_dict_list)
 print()
-print([[value.items()] for value in block_users_dict_list])
+# {key: value for (key, value) in iterable}
+print([{try_dict['_id'].append(value['blockUserId'], value['fromUserId']) for k,v in value.items()} for value in block_users_dict_list])
 # try_dict = {k: f(v) for k, v in my_dictionary.items()}
+
+import time
+
+start_time = time.perf_counter()
 
 try_dict_1 = {}
 for new in block_users_dict_list:
@@ -33,4 +38,6 @@ for new in block_users_dict_list:
     try_dict_1[new['_id']].append(new['blockUserId'])
     try_dict_1[new['_id']].append(new['fromUserId'])
 
+end_time = time.perf_counter()
+print(f'total_time = {end_time-start_time} seconds')
 print(try_dict_1)
